a734961f0a403853d148a1dca9340cf4bb1b8028
Your Name <you@example.com>
2024-03-22 14:46:55 -0500

    Updated at Fri Mar 22 02:46:55 PM CDT 2024

--- sources/run.sh
+++ sources/run.sh
@@ -7,4 +7,4 @@ fi
 DATE="$1"
 
 # Run the command with the provided date
-nohup python main.py --do-pre-train --pre-train-tasks mass --batch-size 32 --eval-batch-size 32 --cuda-visible-devices 0 --fp16 --model-name pre_train --pre-train-subset-ratio 0.001 > output-spt-code-2024-$DATE.log 2>&1 &
+nohup python main.py --do-pre-train --pre-train-tasks mass --batch-size 16 --eval-batch-size 16 --cuda-visible-devices 0 --fp16 --model-name pre_train --pre-train-subset-ratio 0.001 --n-epoch 1 --n-epoch-pre-train 1 > output-spt-code-2024-$DATE.log 2>&1 &
